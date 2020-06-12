dc2621503f2b0f712dfe2107f2d4a174aecc6c37
Filip Troníček <filip.tronicek@seznam.cz>
2020-06-12 13:22:12 +0200

    Twitter

--- script.js
+++ script.js
@@ -15,6 +15,11 @@ function getCount() {
   oReq.open("GET", `https://sponsors.trnck.dev/${user}/count`);
   oReq.send();
 }
+function getTwt(twitter_username) {
+  console.table(twitter_username);
+  if(twitter_username) return '<a href="https://twitter.com/"'+twitter_username+'"><i class="fa fa-twitter"></i></a>';
+  else return "";
+}
 function getSponsors() {
   const oReq = new XMLHttpRequest();
 
@@ -25,8 +30,10 @@ function getSponsors() {
       <li class="sponsor"> <a href="${t.profile}">
         ${t.details.name || t.handle} 
           <br>
-        <img src="${t.avatar}"> 
+            <img src="${t.avatar}"> 
         </a>
+          <br>
+        ${getTwt(t.details.twitter_username)} <br>
       </li>
       `;
     }
